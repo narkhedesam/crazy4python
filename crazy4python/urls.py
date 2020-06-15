5b55a734cd175cbb7ceaaf21deb9ded75baf72ea
Sameer Nakhede <narkhedesam@gmail.com>
2020-06-15 19:31:55 +0530

    changes

--- crazy4python/urls.py
+++ crazy4python/urls.py
@@ -21,5 +21,4 @@ from django.conf.urls.static import static
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('blog.urls')),
-    path('summernote/', include('django_summernote.urls')),
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
