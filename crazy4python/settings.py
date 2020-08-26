907cf5cf4219ace5be48d8f151dc4b99b997ab9e
Sameer Narkhede <narkhedesam@gmail.com>
2020-08-26 10:24:29 +0530

    Update settings.py

--- crazy4python/settings.py
+++ crazy4python/settings.py
@@ -12,7 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 from django.utils.crypto import get_random_string
-# import django_heroku
+import django_heroku
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -162,4 +162,4 @@ LOGGING = {
 
 CRISPY_TEMPLATE_PACK = 'bootstrap4'
 
-# django_heroku.settings(locals())
+django_heroku.settings(locals())
